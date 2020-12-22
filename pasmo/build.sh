e8cfb4f008fc83bd775bc748a978c8cb20aea822
asimakov <asimakov@twelvechairs-software.com>
2020-12-22 01:46:48 +0000

    Added build.sh

---
+++ pasmo/build.sh
@@ -0,0 +1,10 @@
+SOURCE_FILE="$1"
+
+PASMO_PATH="/Users/vokamisair/speccy/pasmo/bin/pasmo"
+
+rm -f "${SOURCE_FILE}".tap
+
+"${PASMO_PATH}" --tapbas "${SOURCE_FILE}".asm "${SOURCE_FILE}".tap
+
+open "${SOURCE_FILE}".tap
+
